61d6613cef215f9cefb85ac077a1ea810bd77efd
Guruprakash Chinnasamy <55480942+guruprakashchinnasamy@users.noreply.github.com>
2023-03-03 17:37:03 +0530

    YamlCode update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.OpenApi.Models;
+using System;
+using System.Net;
+using WebApplication1;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Inventory API", Version = "1.10" });
-    
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "Inventory API",
+        Description = "An API to perform Inventory operations",
+        Version = "2.10" });
+
 });
 var app = builder.Build();
 
@@ -19,6 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Inventory API"));
+     
 }
 
 app.UseHttpsRedirection();
@@ -27,7 +34,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
- 
+app.Lifetime.ApplicationStarted.Register(OnStarted);
 
 
 app.Run();
+
+void OnStarted()
+{
+
+    var wc = new System.Net.WebClient();
+    wc.DownloadFile("https://localhost:7175/swagger/v1/swagger.yaml", @"E:\backstage\backstage-dotnet-webapi\Backstage_App\Backstage_docs\SwaggerInventory.yaml");
+
+}
